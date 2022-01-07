e202b270b9e58db07bb7f2ca4152aa7bc092e59b
HzaRashid <74062092+HzaRashid@users.noreply.github.com>
2022-01-07 11:09:21 -0500

    Add files via upload

--- test_if_works.py
+++ test_if_works.py
@@ -6,7 +6,9 @@ from pathlib import Path
 
 # tsla = Path(__file__).parents[1]/'stock_models/TSLA_model_data.csv'
 
-tsla_file = Path(__file__).parents[1] / 'stock_models/TSLA_model_data.csv'
+# tsla_file = Path(__file__).parents[1] / 'stock_models/TSLA_model_data.csv'
+tsla_file = '/Users/hamzarashid/tensorflow-test/TSLA_model_data.csv'
+# print(tsla_file)
 # nvda_file = Path(__file__).parents[1] / 'stock_models/NVDA_model_data.csv'
 # dis_file = Path(__file__).parents[1] / 'stock_models/DIS_model_data.csv'
 # aapl_file = Path(__file__).parents[1] / 'stock_models/AAPL_model_data.csv'
@@ -56,75 +58,6 @@ def get_data(ticker):
 df = get_data(user_choice_ticker)
 st.subheader(user_choice_ticker)
 st.dataframe(df)
-# @st.cache(allow_output_mutation=True, show_spinner=False)
-# def get_test_data(ticker):
-#     # file = st.file_uploader(ticker + '_model_data.csv')
-#     df = pd.read_csv(ticker + '_model_data.csv')
-#
-#     # df = df.rename(columns={'Close': 'Actual'})
-#     return df
-
-
-# data_load_state = st.text('Training model...')
-# data = get_test_data(user_choice_ticker).rename(columns={'Close': 'Actual'})
-#
-# data_load_state.text('')
-#
-# # plot Actual and Predicted closing prices
-# def plot_data():
-#     fig = go.Figure([
-#         go.Scatter(
-#             x=data.index,
-#             y=data['Actual'],
-#             name='Actual Closing Price',
-#             mode='lines',
-#             line=dict(color='#088F8F')
-#         ),
-#         go.Scatter(
-#             x=data.index,
-#             y=data['Prediction'],
-#             name='Predicted Closing Price',
-#             mode='lines',
-#             line=dict(color='#F08080')
-#         )
-#     ])
-#     fig.layout.update(
-#         title_text='Actual and Predicted Prices ($USD)',
-#         xaxis_title='Date',
-#         yaxis_title='Price ($)',
-#         xaxis_rangeslider_visible=True
-#     )
-#     return fig
-#
-#
-# st.plotly_chart(plot_data())
-#
-#
-# # plot error
-# def plot_error():
-#     fig = go.Figure([
-#         go.Scatter(
-#             x=data.index,
-#             y=data['Error'],
-#             mode='lines',
-#             line=dict(color='#F08080')
-#         )
-#     ])
-#
-#     fig.layout.update(
-#         title_text='Prediction Error (Absolute Difference Between Actual and Predicted Prices, $USD)',
-#         xaxis_title='Date',
-#         yaxis_title='Difference ($)',
-#         xaxis_rangeslider_visible=True
-#     )
-#     return fig
-#
-#
-# st.plotly_chart(plot_error())
-#
-# # show raw data
-# st.subheader('Raw Data ($USD)')
-# st.write(data)
 
 if __name__ == '__main__':
     # tsla = Path(__file__).parents[1] / 'stock_models/TSLA_model_data.csv'
