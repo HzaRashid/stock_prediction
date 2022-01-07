d2d9b461a372dc20ee302a75a84d1a27d9eb9c6b
HzaRashid <74062092+HzaRashid@users.noreply.github.com>
2022-01-07 13:10:43 -0500

    Add files via upload

--- stock_prediction.py
+++ stock_prediction.py
@@ -4,20 +4,14 @@ from keras.models import Sequential
 from plotly import graph_objs as go
 import pandas_datareader as dr
 from datetime import date
-import streamlit as st
 import pandas as pd
 import numpy as np
 
-
-st.title('Visualizing Model Performance')
-st.subheader("Closing Price Predictions")
-
-ticker_options = ['TSLA', 'NVDA', 'DIS', 'AAPL', 'BTC-USD']
-
-user_choice_ticker = st.selectbox('Enter ticker', ticker_options)
-
+# define date range for stock
 start_date = "1950-01-01"
 curr_date = date.today()
+
+# number of days used to make prediction
 trail = 90
 
 # create models
@@ -74,7 +68,6 @@ optimal_models = {'TSLA': model_tsla(),
                   }
 
 
-@st.cache(allow_output_mutation=True, show_spinner=False)
 def get_test_data(ticker):
     # get stock data, only keep the closing prices
     df = dr.DataReader(ticker, 'yahoo', start=start_date, end=curr_date)
@@ -138,63 +131,13 @@ def get_test_data(ticker):
     return data
 
 
-data_load_state = st.text('Training model...')
-ticker_data = get_test_data(user_choice_ticker)
-data_load_state.text('')
+if __name__ == '__main__':
 
-ticker_data = ticker_data.replace(',', '', regex=True)
+    ticker_options = ['TSLA', 'NVDA', 'DIS', 'AAPL', 'BTC-USD']
 
-# plot Actual and Predicted closing prices
-def plot_data():
-    fig = go.Figure([
-        go.Scatter(
-            x=ticker_data.index,
-            y=ticker_data['Actual'],
-            name='Actual Closing Price',
-            mode='lines',
-            line=dict(color='#088F8F')
-        ),
-        go.Scatter(
-            x=ticker_data.index,
-            y=ticker_data['Prediction'],
-            name='Predicted Closing Price',
-            mode='lines',
-            line=dict(color='#F08080')
-        )
-    ])
-    fig.layout.update(
-        title_text='Actual and Predicted Prices ($USD)',
-        xaxis_title='Date',
-        yaxis_title='Price ($)',
-        xaxis_rangeslider_visible=True
-    )
-    return fig
-
-
-st.plotly_chart(plot_data())
-
-# plot error
-def plot_error():
-    fig = go.Figure([
-        go.Scatter(
-            x=ticker_data.index,
-            y=ticker_data['Error'],
-            mode='lines',
-            line=dict(color='#F08080')
-        )
-    ])
-
-    fig.layout.update(
-        title_text='Prediction Error (Absolute Difference Between Actual and Predicted Prices, $USD)',
-        xaxis_title='Date',
-        yaxis_title='Difference ($)',
-        xaxis_rangeslider_visible=True
-    )
-    return fig
-
-
-st.plotly_chart(plot_error())
-
-# show raw data
-st.subheader('Raw Data ($USD)')
-st.write(ticker_data)
+    i = 0
+    for ticker in ticker_options:
+        data = get_test_data(ticker)
+        data.to_csv(ticker + '_model_data.csv')
+        i += 1
+        print(str(i) + '/5 complete')
