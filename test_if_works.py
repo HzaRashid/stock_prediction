abcddda381f399bfb067c5d920bc7d5a7dbd0035
HzaRashid <74062092+HzaRashid@users.noreply.github.com>
2022-01-07 11:15:20 -0500

    Add files via upload

--- test_if_works.py
+++ test_if_works.py
@@ -4,22 +4,9 @@ import streamlit as st
 import pandas as pd
 from pathlib import Path
 
-# tsla = Path(__file__).parents[1]/'stock_models/TSLA_model_data.csv'
+tsla_csv = Path(__file__).parents[1] / 'stock_models/TSLA_model_data.csv'
 
-# tsla_file = Path(__file__).parents[1] / 'stock_models/TSLA_model_data.csv'
-tsla_file = '/Users/hamzarashid/tensorflow-test/TSLA_model_data.csv'
-# print(tsla_file)
-# nvda_file = Path(__file__).parents[1] / 'stock_models/NVDA_model_data.csv'
-# dis_file = Path(__file__).parents[1] / 'stock_models/DIS_model_data.csv'
-# aapl_file = Path(__file__).parents[1] / 'stock_models/AAPL_model_data.csv'
-# btc_file = Path(__file__).parents[1] / 'stock_models/BTC-USD_model_data.csv'
-
-# file:///Users/hamzarashid/tensorflow-test/TSLA_model_data.csv
-# file:///Users/hamzarashid/tensorflow-test/NVDA_model_data.csv
-# file:///Users/hamzarashid/tensorflow-test/DIS_model_data.csv
-# file:///Users/hamzarashid/tensorflow-test/AAPL_model_data.csv
-# file:///Users/hamzarashid/tensorflow-test/BTC-USD_model_data.csv
-file_options = {'TSLA': tsla_file,
+file_options = {'TSLA': tsla_csv,
                 }
 
 st.title('Visualizing Model Performance')
@@ -32,18 +19,6 @@ user_choice_ticker = st.selectbox('Enter ticker', ['TSLA'])
 start_date = "1950-01-01"
 curr_date = date.today()
 trail = 90
-
-# file_options = ('TSLA_model_data.csv',
-#                 'NVDA_model_data.csv',
-#                 'DIS_model_data.csv',
-#                 'AAPL_model_data.csv',
-#                 'BTC-USD_model_data.csv')
-
-# uploaded_files = st.file_uploader('Select data', file_options)
-# for file in uploaded_files:
-#     bytes_data = file.read()
-#     st.write('filename: ', file.name[:-15])
-#     st.write(bytes_data)
 
 @st.cache
 def get_data(ticker):
@@ -65,3 +40,36 @@ if __name__ == '__main__':
     # tsla_file = 'file://' + Path(__file__).parents[1] / 'stock_models/TSLA_model_data.csv'
     data = get_data('TSLA')
     print(data)
+
+
+
+
+
+
+    # file_options = ('TSLA_model_data.csv',
+    #                 'NVDA_model_data.csv',
+    #                 'DIS_model_data.csv',
+    #                 'AAPL_model_data.csv',
+    #                 'BTC-USD_model_data.csv')
+
+    # uploaded_files = st.file_uploader('Select data', file_options)
+    # for file in uploaded_files:
+    #     bytes_data = file.read()
+    #     st.write('filename: ', file.name[:-15])
+    #     st.write(bytes_data)
+    # tsla = Path(__file__).parents[1]/'stock_models/TSLA_model_data.csv'
+
+    # tsla_file = Path(__file__).parents[1] / 'stock_models/TSLA_model_data.csv'
+    # tsla_file = '/Users/hamzarashid/tensorflow-test/TSLA_model_data.csv'
+    # print(tsla_file)
+    # nvda_file = Path(__file__).parents[1] / 'stock_models/NVDA_model_data.csv'
+    # dis_file = Path(__file__).parents[1] / 'stock_models/DIS_model_data.csv'
+    # aapl_file = Path(__file__).parents[1] / 'stock_models/AAPL_model_data.csv'
+    # btc_file = Path(__file__).parents[1] / 'stock_models/BTC-USD_model_data.csv'
+
+    # file:///Users/hamzarashid/tensorflow-test/TSLA_model_data.csv
+    # file:///Users/hamzarashid/tensorflow-test/NVDA_model_data.csv
+    # file:///Users/hamzarashid/tensorflow-test/DIS_model_data.csv
+    # file:///Users/hamzarashid/tensorflow-test/AAPL_model_data.csv
+    # file:///Users/hamzarashid/tensorflow-test/BTC-USD_model_data.csv
+
